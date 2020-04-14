479b3cc52e4831aebf70530763f237cef67c4f75
Grzegorz Marszałek <grzegorz.marszalek.programista@gmail.com>
2020-04-14 23:27:15 +0200

    Updates

---
+++ precache-manifest.9da44a32e1f6d23127b9f2c1c0fb366d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2746f1f4b4710382d873180b128ffd34",
+    "url": "/star-match-game/index.html"
+  },
+  {
+    "revision": "93cdd3c29bf159f10667",
+    "url": "/star-match-game/static/css/main.1b4dc8b8.chunk.css"
+  },
+  {
+    "revision": "05ffdaacbe8ca0bdea0e",
+    "url": "/star-match-game/static/js/2.9d0a61ad.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/star-match-game/static/js/2.9d0a61ad.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "93cdd3c29bf159f10667",
+    "url": "/star-match-game/static/js/main.596b8725.chunk.js"
+  },
+  {
+    "revision": "2a3f95b1f83a0f7f30d1",
+    "url": "/star-match-game/static/js/runtime-main.b001b03a.js"
+  }
+]);
